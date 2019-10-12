7bd2c8cc9b445759b516d45f3a1bad3a14f9b992
James <james.rudd45@gmail.com>
2019-10-12 17:29:44 +1100

    updating HTML pages and corresponding JS

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,26 +1,41 @@
-const statements = [
-  {
-    author: "Officer Parsons",
-    statement: "He put up a hell of a struggle, I'll tell you that."
-  },
-  {
-    author: "Arnold Schwarzenegger",
-    statement: "Who?"
-  },
-  {
-    author: "James's Mother",
-    statement: "He was such a nice boy until the accident"
-  },
-  {
-    author: "Ex-Boss",
-    statement: "Oh God, has he found me?!?"
-  },
-  {
-    author: "Doctor Lewis",
-    statement: "Not suitable for rehabilitation"
-  },
-  {
-    author: "James's Neighbor",
-    statement: "Yeah, guess he's alright"
-  }
-];
+var quoteCycle = function() {
+  var statements = [
+    {
+      author: "Officer Parsons",
+      statement: "He put up a hell of a struggle, I'll tell you that."
+    },
+    {
+      author: "Arnold Schwarzenegger",
+      statement: "Who?"
+    },
+    {
+      author: "James's Mother",
+      statement: "He was such a nice boy until the accident"
+    },
+    {
+      author: "Ex-Boss",
+      statement: "Oh God, has he found me?!?"
+    },
+    {
+      author: "Doctor Lewis",
+      statement: "No, I don't think he's suitable for rehabilitation"
+    },
+    {
+      author: "James's Neighbor",
+      statement: "Yeah, guess he's alright"
+    }
+  ];
+  var choice = statements[Math.floor(Math.random() * statements.length)];
+  console.log(choice);
+  $("#quote").text(choice.statement);
+  $("#quoteAuthor").text("- " + choice.author);
+  $("#quoteBox").css({ opacity: 1 });
+  setTimeout(function() {
+    $("#quoteBox").css({ opacity: 0 });
+  }, 6000);
+};
+
+$(document).ready(function() {
+  quoteCycle();
+  setInterval(quoteCycle, 8000);
+});
